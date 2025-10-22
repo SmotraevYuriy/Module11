b6f97f584e86cdd1c6919cc6abe59e60f4c16827
Smotraev.Yuriy <smotraev.y@gmail.com>
2025-10-22 22:10:20 +0500

    Module11Corrected

--- src/medium2/Main.java
+++ src/medium2/Main.java
@@ -2,19 +2,15 @@ package medium2;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
 
-        HashSet<String> numbersList = new HashSet<>();
-        String numbers = " 1 2 3 3 4 4 5 5 5 7 9 0 8 7 7 9 6";
+        String numbers = "1 2 3 3 4 4 5 5 5 7 9 0 8 7 7 9 6";
         String[] arrayNumbers = numbers.split(" ");
-        for (String number : arrayNumbers) {
-            if (!number.isEmpty()) {
-                numbersList.add(number);
-            }
-        }
-        System.out.println(numbersList.size());
+        Set numberSet = new HashSet<>(Arrays.asList(arrayNumbers));
+        System.out.println(numberSet.size());
 
     }
 }
--- src/medium4/Comparator.java
+++ src/medium4/Comparator.java
@@ -4,10 +4,8 @@ public class Comparator implements java.util.Comparator<String> {
 
     @Override
     public int compare(String country1, String country2) {
-        if (country1.equals("Греция")) {
-            if (!country2.equals("Греция")) {
-                return -1;
-            }
+        if (country1.equals("Греция") && !country2.equals("Греция")) {
+            return -1;
         }
         if (country2.equals("Греция")) {
 
